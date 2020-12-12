9f52500e63aaf310eafa97da30315965e27e63f2
KarlosMontoya91 <karlosmontoya91@gmail.com>
2020-12-11 20:20:40 -0600

    juego

---
+++ Assets/Sprits/Jugador.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Jugador : MonoBehaviour
+{
+    public float fuerzaSalto;
+    public GameManager gameManager;
+
+    private Rigidbody2D rigidbody2D;
+    private Animator animator;
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        rigidbody2D = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            animator.SetBool("estaSaltando", true);
+            rigidbody2D.AddForce(new Vector2(0, fuerzaSalto));
+        }
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Suelo")
+        {
+            animator.SetBool("estaSaltando", false);
+        }
+        if (collision.gameObject.tag == "Obstaculo")
+        {
+            gameManager.gameOver = true; 
+        }
+    }
+}
